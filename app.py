8e92dabbd32137a7a842186e922fdf9adaefc202
Nishanth Prem <nishanthprem8@gmail.com>
2024-05-13 00:24:26 -0700

    Minor code changes.

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
-    pass
+    return render_template('index.html')
+
+@app.route('/submit', methods=['POST'])
+def url():
+    if request.method == 'POST':
+        input_value = request.form['input']
+        return f'The input value is: {input_value}'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
