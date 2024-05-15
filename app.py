55c53882b9b682e0e373dfbf8c2745f987fc2742
Nishanth Prem <nishanthprem8@gmail.com>
2024-05-14 22:57:53 -0700

    Frontend tweaks.

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template, request
+import feedparser
 
+from flask import Flask, render_template, request
+from urllib.parse import urlparse
 app = Flask(__name__)
 
 
@@ -9,10 +11,29 @@ def home():
 
 
 @app.route('/submit', methods=['POST'])
-def url():
+def submit():
     if request.method == 'POST':
-        input_value = request.form['input']
-        return f'The input value is: {input_value}'
+        try:
+            # Get input from the user and parse it
+            user_input = request.form['input']
+            url_data = urlparse(user_input)
+
+            """
+            `scheme`: protocol used 'http' or 'https'
+            `netloc`:   contains the network location of the resource.
+            """
+            if not (url_data.scheme and url_data.netloc):  
+                raise ValueError()   # Raise error if invalid URL
+            output = {}
+            feed = feedparser.parse(user_input)
+            for entry in feed.entries:
+                output["Title"] = entry.title
+                output["Link"] = entry.links
+                output["Description"] = entry.description
+            return f"{output}"
+
+        except Exception as e:
+            return f"Invalid URL, {e}"
 
 
 if __name__ == '__main__':
